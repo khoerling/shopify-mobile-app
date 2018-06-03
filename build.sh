caa16dcf1f6d533c2b0d445bef4617e0fb42791c
mr. keith <keith@hoerling.com>
2018-06-02 18:05:51 -0700

    adds build script for our forks

---
+++ build.sh
@@ -0,0 +1,6 @@
+# copy local repos
+rm -rf node_modules/react-native-modalbox && cp -pR ../react-native-modalbox node_modules/.
+rm -rf node_modules/react-native-bottom-drawer && cp -pR ../react-native-bottom-drawer node_modules/
+rm -rf node_modules/react-native-parabolic && cp -pR ../react-native-parabolic node_modules/
+
+# TODO build config.js
