8ef6c277a770d9501a24651998902de7db6c918f
mr. keith <keith@hoerling.com>
2018-06-09 22:14:41 -0700

    adds our custom parallax-swiper to build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 rm -rf node_modules/react-native-modalbox && cp -pR ../react-native-modalbox node_modules/.
 rm -rf node_modules/react-native-bottom-drawer && cp -pR ../react-native-bottom-drawer node_modules/
 rm -rf node_modules/react-native-parabolic && cp -pR ../react-native-parabolic node_modules/
+rm -rf node_modules/react-native-parallax-swiper && cp -pR ../react-native-parallax-swiper node_modules/
 
 # TODO build config.js
