bf15cb31f773523de6a98f073b0108450fa789a6
mr. keith <keith@hoerling.com>
2018-06-17 22:09:57 -0700

    fade in/out story titles

--- src/GridItem.js
+++ src/GridItem.js
@@ -1,29 +1,60 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native'
+import { Animated, Easing, StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native'
 import PhotoGallery from './PhotoGallery'
 import { LinearGradient } from 'expo'
 
-const Item = ({ item, onPhotoOpen }) =>
-  <TouchableWithoutFeedback onPress={() => onPhotoOpen(item)}>
-    <View style={{paddingBottom: 20}}>
-      <PhotoGallery.Photo
-        photo={item}
-        style={{
-          width: item.width,
-          height: item.height
-        }}
-      />
-      <LinearGradient colors={['transparent', 'rgba(0,0,0,.3)', 'rgba(0,0,0,.9)', 'rgba(0,0,0,.99)']} style={styles.gradient}>
-        <View>
-          <Text style={styles.h1}>{item.title}</Text>
-          <Text style={styles.h2}>{item.genre.toUpperCase()}</Text>
-        </View>
-      </LinearGradient>
-    </View>
-  </TouchableWithoutFeedback>
+const Item = class Item extends React.Component {
+  state = {
+    opacity: new Animated.Value(1),
+  }
+  componentWillMount() {
+    bus.addListener('storySelected', _ => {
+      // build-out titles
+      this.state.opacity.setValue(0)
+    })
+    bus.addListener('photoGalleryClosed', photoId => {
+      // build-in titles
+      Animated.timing(this.state.opacity, {
+        toValue: 1,
+        duration: 800,
+        easing: Easing.easeOutExpo,
+        useNativeDriver: true
+      }).start()
+    })
+  }
+
+  render() {
+    const { item, onPhotoOpen } = this.props
+    return (<TouchableWithoutFeedback onPress={() => onPhotoOpen(item)}>
+      <View>
+        <PhotoGallery.Photo
+          photo={item}
+          style={{
+            width: item.width,
+            height: item.height
+          }}
+        />
+        <Animated.View style={[styles.container, {opacity: this.state.opacity}]}>
+          <LinearGradient colors={['transparent', 'rgba(0,0,0,.3)', 'rgba(0,0,0,.9)', 'rgba(0,0,0,.99)']} style={styles.gradient}>
+            <View>
+              <Text style={styles.h1}>{item.title}</Text>
+              <Text style={styles.h2}>{item.genre.toUpperCase()}</Text>
+            </View>
+          </LinearGradient>
+        </Animated.View>
+      </View>
+    </TouchableWithoutFeedback>)
+  }
+}
 
 
 const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
   h1: {
     fontSize: 25,
     lineHeight: 24,
@@ -44,14 +75,10 @@ const styles = StyleSheet.create({
   },
   gradient: {
     flex: 1,
-    alignItems: 'flex-start',
-    position: 'absolute',
-    backgroundColor: 'transparent',
     paddingTop: 150,
     paddingLeft: 10,
-    bottom: 0,
-    left: 0,
-    right: 0,
+    alignItems: 'flex-start',
+    backgroundColor: 'transparent',
   },
 })
 export default Item
